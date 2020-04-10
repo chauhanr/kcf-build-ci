234c08f68abefa3134480c59a70b19ba8a4fdf42
root <root@uaa-build.HCL-America-Inc-Shiva-BDF-1.cloud>
2020-04-10 04:35:12 +0000

    tar

--- pipelines/release-cf-deployment-images/tasks/build.sh
+++ pipelines/release-cf-deployment-images/tasks/build.sh
@@ -22,7 +22,7 @@ trap 'stop_docker' EXIT
 echo "${DOCKER_TEAM_PASSWORD_RW}" | docker login "${DOCKER_REGISTRY}" --username "${DOCKER_TEAM_USERNAME}" --password-stdin
 
 # Extract the fissile binary.
-tar xvf fissile-*.tar.gz --directory "/usr/local/bin/"
+tar xvf ./s3.fissile-linux/fissile-4.3.tar.gz --directory "/usr/local/bin/"
 
 # Pull the stemcell image.
 stemcell_version="${STEMCELL_VERSION}""
